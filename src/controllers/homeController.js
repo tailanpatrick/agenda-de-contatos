e8c66f446cf37a0c9690817bc3ad53aa5a6d9cdb
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-07-11 14:55:43 -0300

    alterações na homepage e seu controller e tambem na view de contatos

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,13 @@
 const HomeModel = require('../models/HomeModel');
 
 exports.paginaInicial = (req, res, next) => {
+    const auth = true;
+
     res.render('index', {
-        titulo: 'Este será o título da página', 
-        numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        titulo: 'Este será o título da página',
+        auth: auth
     });
+
     return;
 }
 
