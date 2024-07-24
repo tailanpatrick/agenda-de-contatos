f5607117382c6aacd93747a5c95ce2475586ba37
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-07-24 15:58:17 -0300

    ajustada a posição do botão adicionar contato

--- src/views/includes/components/ButtonAddContact.jsx
+++ src/views/includes/components/ButtonAddContact.jsx
@@ -3,7 +3,7 @@ const React = require('react');
 function ButtonAddContact() {
     return (
 
-        <div className="absolute bottom-20 right-5 md:right-28 bg-[#0D7DC0] text-center px-2 py-2 rounded-full text-white hover:bg-[#0AABF4] text-lg cursor-pointer">
+        <div className="absolute bottom-10 right-5 md:right-28 bg-[#0D7DC0] text-center px-2 py-2 rounded-full text-white hover:bg-[#0AABF4] text-lg cursor-pointer">
             <img className="w-10" src="/assets/img/mais.png"/>
         </div>
 
